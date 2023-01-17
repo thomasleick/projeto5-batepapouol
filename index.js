2274b3ab6b378d80eecb4a8ec984bd39a32e28d4
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-01-17 19:21:12 -0300

    join room

---
+++ index.js
@@ -0,0 +1,25 @@
+// side menu
+
+function openMenu() {
+    document.getElementById("container").classList.remove("hidden");
+};
+
+function closeMenu() {
+    document.getElementById("container").classList.add("hidden");
+};
+
+
+// API`s
+let stillOnline = null;
+
+function join(usr) {
+    const baseURL = "https://mock-api.driven.com.br/api/v6/uol/participants";
+    const user = { name: `"${usr}"` };
+     axios.post(baseURL, user).then((response) => {
+        console.log(response);
+        stillOnline = setInterval(function () {ping(usr)}, 5000);
+    })
+    .catch ((err) => {
+        console.log(err);
+    });
+};
