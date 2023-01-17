13950dc09e52f4f16e5ad507a8b33fc2c6d1a00d
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-01-17 19:21:36 -0300

    keep connection

--- index.js
+++ index.js
@@ -23,3 +23,18 @@ function join(usr) {
         console.log(err);
     });
 };
+
+function ping(usr) {
+    const baseURL = "https://mock-api.driven.com.br/api/v6/uol/status"
+    const user = { name: `"${usr}"` };
+
+    axios.post(baseURL, user).then((response) => {
+        console.log(response);
+    })
+    .catch ((err) => {
+        console.log(err)
+    });
+};
+
+join("Thom");
+setTimeout(function () {clearInterval(stillOnline)}, 15000);
