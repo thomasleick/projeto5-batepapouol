0a70b36e244979bfa2477f83abce5892e7e8813d
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-01-17 19:57:08 -0300

    send message

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ let stillOnline = null;
 
 function join(usr) {
     const baseURL = "https://mock-api.driven.com.br/api/v6/uol/participants";
-    const user = { name: `"${usr}"` };
+    const user = { name: `${usr}` };
      axios.post(baseURL, user).then((res) => {
         console.log(res);
-        getMsg();
+        setTimeout(function () { 
+            sendMsg(usr, "Todos", "Testando minha API novamente", "message");
+            getMsg();
+        }, 2000);
+        
         stillOnline = setInterval(function () {ping(usr)}, 5000);
     })
     .catch ((err) => {
@@ -27,7 +31,7 @@ function join(usr) {
 
 function ping(usr) {
     const baseURL = "https://mock-api.driven.com.br/api/v6/uol/status";
-    const user = { name: `"${usr}"` };
+    const user = { name: `${usr}` };
 
     axios.post(baseURL, user).then((res) => {
         console.log(res);
@@ -48,5 +52,19 @@ function getMsg() {
     });
 };
 
-join("Thom");
+function sendMsg(from, to, text, type) {
+    const params = { from: `${from}`, to: `${to}`, text: `${text}`, type: `${type}` };
+    const baseURL = "https://mock-api.driven.com.br/api/v6/uol/messages";
+
+    axios.post(baseURL, params).then((res) => {
+        console.log(res);
+    })
+    .catch ((err) => {
+        console.log(err);
+    });
+};
+
+
+const usr = "Thom";
+join(usr);
 setTimeout(function () {clearInterval(stillOnline)}, 15000);
